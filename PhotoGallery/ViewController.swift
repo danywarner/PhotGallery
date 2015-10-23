fd84a47481bdf8364a2e103cafda0784a4efcc91
Daniel Warner <danywarner@gmail.com>
2015-10-23 11:36:02 -0500

    Initial Commit

---
+++ PhotoGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoGallery
+//
+//  Created by Daniel Warner on 10/23/15.
+//  Copyright © 2015 Daniel Warner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
